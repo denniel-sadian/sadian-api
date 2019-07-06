471a69c660d9858f9488ea636bdb612dbc0acca5
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-07-06 20:43:07 +0800

    Remove not needed codes

--- download_data.py
+++ download_data.py
@@ -18,14 +18,6 @@ for p in projects:
         description=p['description']
     )
 
-    pub_date = models.DateTimeField()
-    status = models.CharField(max_length=255, choices=STATUS)
-    can_comment = models.BooleanField()
-    image = models.URLField(blank=True)
-    content = models.TextField()
-    preview_content = models.TextField()
-    views = models.IntegerField(default=0)
-
 for e in entries:
     Entry.objects.create(
         headline=e['headline'],
